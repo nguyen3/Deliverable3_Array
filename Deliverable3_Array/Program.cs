78dd2048c15adf2e5edab78d104d8b3ab3ca7e31
nguyen3 <nguyen33@mail.usf.edu>
2019-05-02 15:49:02 -0400

    Update the Deliverable 3 Array

--- Deliverable3_Array/Program.cs
+++ Deliverable3_Array/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+    Author: Trang Nguyen
+    Date: 1/4/2019
+    Comments: This C# Console application code demonstrates the use of
+              Arrays to display 25 elements
+*/
+
+using System;
 
 namespace Deliverable3_Array
 {
@@ -6,7 +13,27 @@ namespace Deliverable3_Array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            Console.WriteLine("Display array of 25 elements:");
+            Console.WriteLine("_________________________________________"); //Add horizontal line
+            Console.WriteLine("");//Add empty line
+
+            //Assign number of elements
+            int number_of_elements = 25;
+
+            // Automatically populate an array with a number of elements
+            int[] element = new int [number_of_elements];
+
+            // Iterate through the array using a For Loop
+            for (int i = 1; i <= number_of_elements; i++)
+            {
+                Console.WriteLine("Element value = " + i);
+            }//End of For
+
+            Console.WriteLine("");//Add empty line
+            Console.WriteLine("_________________________________________");//Add horizontal line
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey(true);
+        }//End of Main
+    }//End of Class
+}//End of namespace
